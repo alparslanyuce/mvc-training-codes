5a17c5c227a3a50ffee10b1a9c1677b2aec7c174
Alparslan Yüce <alparslan.yuce@icloud.com>
2023-10-23 22:31:57 +0300

    first commit

---
+++ coreproje/Controllers/OrnekController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace coreproje.Controllers
+{
+    public class Product2
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+
+    public class OrnekController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.name = "Asp.Net Core";
+            ViewData["age"] = 30;
+            ViewData["names"] = new List<string>() { "alparslan", "simge" };
+            ViewBag.person = new { id = 1, name = "alparslan", age = 25 };
+            var productList = new List<Product2>()
+            {
+            new (){Id=1, Name ="alparslan"},
+            new (){Id=2, Name ="simge"}
+            };
+            return View(productList);
+        }
+
+        public IActionResult Index3()
+        {
+            TempData["surname"] = "yüce";  
+            return View();
+        }
+
+        //redirectoaction kullanımı
+        public IActionResult Index2()
+        {
+            return RedirectToAction("Index","Ornek");
+        }
+
+        //parametre tanımlama 
+        public IActionResult ParametreView(int id)
+        {
+            return RedirectToAction("JsonResultParametre", "Ornek", new { id = id });
+        }
+
+
+        public IActionResult JsonResultParametre(int id)
+        {
+            return Json(new { Id = id});
+        }
+
+        //contentresult kullanımı
+        public IActionResult ContentResult()
+        {
+            return Content("contentresult");
+        }
+
+        //json result kullanımı
+        public IActionResult JsonResult()
+        {
+            return Json(new { id = 1,name= "kalem", price = 100 });
+        }
+
+        //empty result kullanımı
+        public IActionResult EmptyResult()
+        {
+            return new EmptyResult();
+        }
+    }
+}
---
+++ coreproje/Models/ProductRepository.cs
@@ -0,0 +1,61 @@
+﻿namespace coreproje.Models
+{
+	public class ProductRepository
+	{
+		private static List<Product> _products = new List<Product>()
+		{
+		new()
+				{
+					Id = 1,
+					Name = "kalem1",
+					Price = 100,
+					Stock =300
+				},
+		new ()
+				{
+					Id = 2,
+					Name = "kalem2",
+					Price = 200,
+					Stock = 500
+				}
+		};
+
+		public List<Product> GetAll() => _products;
+
+		public void AddProduct(Product newProduct) => _products.Add(newProduct);
+
+		public void Remove(int id)
+		{
+			var hasproduct = _products.FirstOrDefault(x => x.Id == id);
+
+			if(hasproduct != null)
+			{
+				_products.Remove(hasproduct);
+			}
+			else
+			{
+				throw new Exception($"bu id({id}'ye sahip ürün bulunamamıştır.)");
+			}
+		}
+
+		public void Update(Product updateProduct)
+		{
+			var hasproduct = _products.FirstOrDefault(x=> x.Id== updateProduct.Id);
+			if( hasproduct != null)
+			{
+				hasproduct.Name =updateProduct.Name;
+				hasproduct.Price=updateProduct.Price;
+				hasproduct.Stock=updateProduct.Stock;
+
+				var index=_products.FindIndex(x=> x.Id == updateProduct.Id);
+				_products[index] = hasproduct;
+			}
+			else
+			{
+				throw new Exception($"bu id({updateProduct.Id}'ye sahip ürün bulunamamıştır.)");
+			}
+		}
+		
+
+	}
+}
